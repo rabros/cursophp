323db9fc488186d3ccd2277cede1e61a00205929
João Barros <joaopilzbarros@gmail.com>
2023-03-09 11:01:20 -0300

    Create index.php

---
+++ Variaveis e constante/index.php
@@ -0,0 +1,13 @@
+<?php 
+    $nome = "Cézar";
+    $sobrenome = "Barros";
+    const PAIS = "Brasil"; // constante 
+   
+
+    $nome = "João"; // variavel
+    // deixou de ser Cezar para João
+    echo "Muito prazer, $nome $sobrenome! Você mora no " . PAIS ;
+
+    //$NomeCompletoALuno = "Came Case"
+    //%nome_completo_aluno = "Snake Case"
+?>
