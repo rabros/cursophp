d091709907d50ff999df5b3032857ffd7d209ea0
João Barros <105664722+rabros@users.noreply.github.com>
2023-03-10 15:14:06 -0300

    Update index.php

--- Variaveis e constante/index.php
+++ Variaveis e constante/index.php
@@ -14,4 +14,19 @@
     //TIPOS DE DADOS
 
     //string - number (interger, float(double)) - boolean - arrays - object - null
+    
+    // 0x - hexadecimal, 0b - binario, 0 - octa
+    // $num = 010;
+    // echo "O valor da variavel é $num";
+
+    // $v = 300;
+    // var_dump($v);
+    // var_dump vai mostrar o tipo primitivo e o valor dela
+
+    // $num = (integer) 3e10 // 3 * 10^2
+    // coersão "força a varivel virar o que está dentro do parenteses" o exemplo acima por padrão é float com "(integer)" virou inteiro
+
+    // $vet = [1, 2, 3, 4, 6]
+    // var_dump($vet) // array
+    
 ?>
