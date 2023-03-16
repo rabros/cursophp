53691311dc9eca5905e884023f2a507335b973c9
João Barros <joaopilzbarros@gmail.com>
2023-03-16 10:48:51 -0300

    Desafios

---
+++ desafios/desafio03/conversor.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Resultado Desafio 3</title>
+</head>
+<body>
+    <main>
+        <h1>Conversor de Moedas v1.0</h1>
+        <?php
+        $number = $_GET["number"];
+        $resultadoConversao = number_format(("$number") / 5.28, 2);
+        echo "Seus R$ $number equivalem a U$$ <b>$resultadoConversao<b>";
+        // Falta deixar o $number aceitar em numero decimal
+        ?>
+        <p> <strong>Cotação fixa de R$5.27</strong> informada direto no codigo</p> 
+        <button><a href="javascript:history.go(-1)">Voltar</a></button>
+    </main>  
+</body>
+</html>
