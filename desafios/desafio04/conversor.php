b822c649fe90b72651183057525ea7805d8bb956
João Barros <105664722+rabros@users.noreply.github.com>
2023-03-23 13:13:34 -0300

    Update conversor.php

--- desafios/desafio04/conversor.php
+++ desafios/desafio04/conversor.php
@@ -12,17 +12,19 @@
     <main>
         <h1>Conversor de Moedas v2.0</h1>
         <?php
-            $number = $_GET["number"];
+            
+            $number = $_GET["number"] ?? 0;
             $url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial='03-09-2023'&@dataFinalCotacao='03-15-2023'&top=100&format=json";
             $cotacao = json_decode(file_get_contents($url));
+            $padrao = numfmt_create("pt_BR", number_format::CURRENCY);
             // var_dump($cotacao);            
             foreach($cotacao->value as $valor){
                 $ultimaCotacao = $valor->cotacaoVenda;//PEGAR A ULTIMA COTAÇÃO PARA FAZER O CALCULO E COM O FAZER O CALCULO            
             }
-            //Verificar também para deixar colocar numero real
             // var_dump($ultimaCotacao);
-            $valorConvertido = number_format(("$number" / "$ultimaCotacao"), 2);
-            echo "U$$ $valorConvertido";
+            $conversao = $number / $ultimaCotacao;
+            // $valorConvertido = number_format(("$number" / "$ultimaCotacao"), 2);
+            echo "<p>Seus " . numfmt_format_currency($padrao, $number, "BRL") . "equivalem a <strong>" . numfmt_format_currency($padrao, $conversao, "USD") . "</strong></p>";
         ?>
         <p>Valor da cotação pego do <strong>Banco Central</strong></p>
         <button><a href="javascript:history.go(-1)">Voltar</a></button>
