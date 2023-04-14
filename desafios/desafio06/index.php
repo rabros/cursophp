7067fbf1accbfa61c4dee35d249ef9124a89fa71
João Barros <joaopilzbarros@gmail.com>
2023-04-13 22:18:37 -0300

    Update index.php

--- desafios/desafio06/index.php
+++ desafios/desafio06/index.php
@@ -11,14 +11,14 @@
    <main>
         <?php 
             $valor1 = $_GET["dividendo"]??0;
-            $valor2 = $_GET["divisor"]??0;
+            $valor2 = $_GET["divisor"]??1;
         ?>
         <h1>Anatomia de uma Divisão</h1>
         <form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
             <label for="dividendo">Dividendo</label>
-            <input type="number" name="dividendo" id="dividendo" value="<?=$valor1?>">
+            <input type="number" name="dividendo" id="dividendo" min="0" value="<?=$valor1?>">
             <label for="divisor">Divisor</label>
-            <input type="number" name="divisor" id="divisor" value="<?=$valor2?>">
+            <input type="number" name="divisor" id="divisor" min="1" value="<?=$valor2?>">
             <input type="submit" value="Analisar">
         </form>
     </main> 
@@ -27,9 +27,18 @@
        <?php 
             $resto_div = $valor1 % $valor2;
             $inteiro_div = (int)($valor1/$valor2);
-            echo "<p>$valor1 | $valor2</p>";
-            echo "<p><u>$resto_div</u> | $inteiro_div</p>"
         ?>
+
+        <table class="divisao">
+            <tr>
+                <td><?=$valor1?></td>
+                <td><?=$valor2?></td>
+            </tr>
+            <tr>
+                <td><?=$resto_div?></td>
+                <td><?=$inteiro_div?></td>
+            </tr>
+        </table>
     </section>
 </body>
 </html>
