8126ab974a6f96986e0a5e04725cd3cec988c634
João Barros <105664722+rabros@users.noreply.github.com>
2023-05-26 13:12:05 -0300

    Update index.php

--- desafios/desafio10/index.php
+++ desafios/desafio10/index.php
@@ -8,15 +8,16 @@
 </head>
 <body>
     <?php
+        $ano = $GET['ano_atual'] ?? $ano_atual;
         $ano_nasc = $_GET['ano_nasc'] ?? 2000;
-        $ano_atual = $GET['ano_atual'] ?? date['Y'];
+        $ano_atual = date['Y'];
     ?>
     <main>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="number">Digite seu ano de nascimento</label>
             <input type="number" name="ano_nasc" id="ano_nasc" value="<?=$ano_nasc?>">
-            <label for="number">Quer saber sua idade em que ano? (Atualmente estamos em $ano_atual)</label>
-            <input type="number" name="ano_atual" id="ano_atual" value="<?=$ano_atual?>">
+            <label for="number">Quer saber sua idade em que ano? (Atualmente estamos em $ano)</label>
+            <input type="number" name="ano" id="ano_atual" value="<?=$ano?>">
             <input type="submit" value="Calcular idade">
         </form>
     </main> 
